80fe90a55fa676664df31705343e3d285615723f
Konrad Gawryluk <konrad.gawryluk98@gmail.com>
2022-12-01 14:33:47 +0100

    Task nr 1

---
+++ 1/index.ts
@@ -0,0 +1,11 @@
+const __dirname = new URL('.', import.meta.url).pathname;
+const text = Deno.readTextFileSync(`${__dirname}input.txt`).toString();
+const arrayOfArrays = text
+	.split('\n\n')
+	.filter((text) => text !== '')
+	.map((line) => line.split('\n'))
+const sumArrayValues = (array) => array.reduce((acc, value) => acc + parseInt(value), 0);
+const arrayOfCaloriesSum = arrayOfArrays.map(sumArrayValues).sort((a, b) => b - a)
+
+console.log(`A: ${sumArrayValues(arrayOfCaloriesSum.slice(0, 1))}`)
+console.log(`B: ${sumArrayValues(arrayOfCaloriesSum.slice(0, 3))}`)
